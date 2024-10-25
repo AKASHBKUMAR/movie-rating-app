c72583ca31e9dacc92b1b6fdfad116901152af4a
Akash Kumar B <akashbkumar30@gmail.com>
2024-10-25 16:41:18 +0530

    Set the Environmental Variables

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 // IMPORT NODE PACKAGES
 const express = require("express");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
 
-const PORT = 3500;
+dotenv.config({path:"./config.env"});
+
+const PORT = process.env.PORT || 3500;
 
 // CALLING THE FUNCTION
 const app = express();
 
+// console.log(app.get('env'))
+// console.log(process.env);
 
 const movieRouter = require("./Routes/MovieRoutes");
 
 
 app.use(express.json());
-app.use(morgan("dev"));
+if(process.env.NODE_ENV === "Development")
+  app.use(morgan("dev"));
 app.use(express.static("./Public"));
 
 app.use("/api/v1/movies", movieRouter);
 
-app.listen(PORT,()=>{console.log("Server has started in localhost:3500")});
+app.listen(PORT,()=>{console.log(`Server has started in localhost:${process.env.PORT}`)});
