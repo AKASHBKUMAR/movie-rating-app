e9b8be71b93d9e5dc5b148981e09361dc83b098b
Akash Kumar B <akashbkumar30@gmail.com>
2024-10-26 10:05:50 +0530

    Created a Model for Movie

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 // IMPORT NODE PACKAGES
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({path:"./config.env"});
 
 const PORT = process.env.PORT || 3500;
+mongoose.connect(process.env.CONNECTION_STRING,{useNewUrlParser:true})
+  .then((connection)=>{console.log("DB Connection Successful")})
+  .catch((error)=>{console.log(error)});
 
+const movieSchema = new mongoose.Schema({
+  name:{type:String,required:[true,"Name is a Required Field"],unique:true} ,
+  descrption: String,
+  duration:{type:Number,required:[true,"Duration is a required Field"]},
+  ratings: {type:Number,default:1.0}
+
+})
+
+const Movie = mongoose.model("Movies",movieSchema);
 // CALLING THE FUNCTION
 const app = express();
 
-// console.log(app.get('env'))
-// console.log(process.env);
+const testMovie = new Movie({name:"Peaky Blinders",description:"Action Packed Brutal way of potraying Shelby",duration:120,ratings:5})
+testMovie.save().then((document)=>{console.log(document)}).catch((error)=>{console.log(error)});
 
 const movieRouter = require("./Routes/MovieRoutes");
 
